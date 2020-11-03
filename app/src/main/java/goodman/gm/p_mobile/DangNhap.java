70e472481f4c880f8234cb7ba84a71a755dc180e
Quang16599 <58855093+Quang16599@users.noreply.github.com>
2020-11-03 19:10:00 +0700

    a

--- app/src/main/java/goodman/gm/p_mobile/DangNhap.java
+++ app/src/main/java/goodman/gm/p_mobile/DangNhap.java
@@ -2,13 +2,26 @@ package goodman.gm.p_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DangNhap extends AppCompatActivity {
 
+    Button btnDangKy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
+
+        btnDangKy = findViewById(R.id.dk);
+        btnDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DangNhap.this, DangKy.class);
+                startActivity(intent);
+            }
+        });
     }
 }
