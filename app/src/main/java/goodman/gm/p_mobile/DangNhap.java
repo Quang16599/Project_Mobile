839b2d90dbf5b51468271e68e28851beb7427449
quangnhatdepvl <55266938+quangnhatdepvl@users.noreply.github.com>
2020-11-04 18:54:30 +0700

    zzzzz

--- app/src/main/java/goodman/gm/p_mobile/DangNhap.java
+++ app/src/main/java/goodman/gm/p_mobile/DangNhap.java
@@ -7,13 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class DangNhap extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    Button btnDangKy;
+public class DangNhap extends AppCompatActivity {
+    FirebaseDatabase  rootNode;
+    DatabaseReference reference;
+    Button btnDangKy,btnDangnhap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
+        btnDangnhap = findViewById(R.id.btnLogin);
+        btnDangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("user");
+                reference.setValue("dmmm");
+            }
+        });
 
         btnDangKy = findViewById(R.id.btnRegiste);
         btnDangKy.setOnClickListener(new View.OnClickListener() {
