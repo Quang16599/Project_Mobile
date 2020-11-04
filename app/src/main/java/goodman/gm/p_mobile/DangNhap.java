110195282abad5da8d5f9b3f0c9431352f6eac46
quangnhatdepvl <55266938+quangnhatdepvl@users.noreply.github.com>
2020-11-04 13:29:31 +0700

    zzz

--- app/src/main/java/goodman/gm/p_mobile/DangNhap.java
+++ app/src/main/java/goodman/gm/p_mobile/DangNhap.java
@@ -15,7 +15,7 @@ public class DangNhap extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
 
-        btnDangKy = findViewById(R.id.dk);
+        btnDangKy = findViewById(R.id.btnRegiste);
         btnDangKy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
