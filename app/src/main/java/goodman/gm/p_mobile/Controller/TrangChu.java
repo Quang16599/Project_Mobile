baccbc739587d4b3512514ebf182a831a06d632f
quangnhatdepvl <55266938+quangnhatdepvl@users.noreply.github.com>
2020-11-13 23:33:09 +0700

    aa

--- app/src/main/java/goodman/gm/p_mobile/Controller/TrangChu.java
+++ app/src/main/java/goodman/gm/p_mobile/Controller/TrangChu.java
@@ -1,16 +1,73 @@
 package goodman.gm.p_mobile.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import goodman.gm.p_mobile.Adapter.AdapterViewPagerTrangChu;
 import goodman.gm.p_mobile.R;
 
-public class TrangChu extends AppCompatActivity {
-
+public class TrangChu extends AppCompatActivity implements ViewPager.OnPageChangeListener, RadioGroup.OnCheckedChangeListener {
+    ViewPager viewPagerTrangChu;
+    RadioButton rdOdau, rdAnGi;
+    RadioGroup rdGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trang_chu);
+        init();
+        AdapterViewPagerTrangChu adapterViewPagerTrangChu =  new AdapterViewPagerTrangChu(getSupportFragmentManager());
+        viewPagerTrangChu.setAdapter(adapterViewPagerTrangChu);
+        viewPagerTrangChu.addOnPageChangeListener(this);
+        rdGroup.setOnCheckedChangeListener(this);
+    }
+
+    private void init() {
+        viewPagerTrangChu = (ViewPager) findViewById(R.id.viewPagerTrangChu);
+        rdOdau = (RadioButton) findViewById(R.id.rdOdau);
+        rdAnGi = (RadioButton) findViewById(R.id.rdAnGi);
+        rdGroup = (RadioGroup) findViewById(R.id.rdGroup);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+// dung de chuyen nut o dau sang an gi
+     switch (position){
+         case 0:
+             rdOdau.setChecked(true);
+             break;
+         case 1 :
+             rdAnGi.setChecked(true);
+             break;
+     }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId){
+            case R.id.rdOdau:
+                viewPagerTrangChu.setCurrentItem(0);
+                break;
+            case R.id.rdAnGi:
+                viewPagerTrangChu.setCurrentItem(1);
+                break;
+
+
+        }
     }
 }
---
+++ app/src/main/java/goodman/gm/p_mobile/Fragments/OdauFragment.java
@@ -0,0 +1,23 @@
+package goodman.gm.p_mobile.Fragments;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import goodman.gm.p_mobile.R;
+
+public class  OdauFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_fragment_odau,container,false);
+
+        return view;
+    }
+}
