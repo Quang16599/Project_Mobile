44bfec2b83391fd2868f1deae84a695d7f87f578
quangnhatdepvl <55266938+quangnhatdepvl@users.noreply.github.com>
2020-12-29 22:44:04 +0700

    abc

--- app/src/main/java/goodman/gm/p_mobile/Controller/TrangChu.java
+++ app/src/main/java/goodman/gm/p_mobile/Controller/TrangChu.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
+import com.smarteist.autoimageslider.SliderLayout;
+import com.smarteist.autoimageslider.SliderView;
 
 import goodman.gm.p_mobile.Adapter.AdapterViewPagerTrangChu;
 import goodman.gm.p_mobile.Model.User;
@@ -21,15 +26,47 @@ public class TrangChu extends AppCompatActivity implements ViewPager.OnPageChang
     RadioGroup rdGroup;
     Button btnGanToi;
     ImageButton imbPerson;
+    SliderLayout sliderLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trang_chu);
         init();
+        setSliderViews();
 
         controlButton();
         receiveData();
+    }
+
+    private void setSliderViews() {
+
+        for (int i = 0; i <= 3; i++) {
+
+            SliderView sliderView = new SliderView(this);
+
+            switch (i) {
+                case 0:
+                    sliderView.setImageDrawable(R.drawable.banner);
+                    break;
+                case 1:
+                    sliderView.setImageDrawable(R.drawable.banner2);
+                    break;
+                case 2:
+                    sliderView.setImageDrawable(R.drawable.banner3);
+                    break;
+                case 3:
+                    sliderView.setImageDrawable(R.drawable.banner4);
+                    break;
+            }
+
+            sliderView.setImageScaleType(ImageView.ScaleType.CENTER_CROP);
+            final int finalI = i;
+
+            sliderLayout.addSliderView(sliderView);
+        }
+
     }
 
 
@@ -67,6 +104,11 @@ public class TrangChu extends AppCompatActivity implements ViewPager.OnPageChang
         viewPagerTrangChu.setAdapter(adapterViewPagerTrangChu);
         viewPagerTrangChu.addOnPageChangeListener(this);
         rdGroup.setOnCheckedChangeListener(this);
+        sliderLayout = findViewById(R.id.imageSlider);
+        sliderLayout.setIndicatorAnimation(SliderLayout.Animations.FILL); //set indicator animation by using SliderLayout.Animations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderLayout.setScrollTimeInSec(1); //set scroll delay in seconds :
+
+
     }
 
     @Override
