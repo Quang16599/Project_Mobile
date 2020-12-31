1653e69f8453ec8c6a0f00380d614ee4327c0180
Quang16599 <58855093+Quang16599@users.noreply.github.com>
2020-12-31 11:04:01 +0700

    google map

---
+++ app/src/main/java/goodman/gm/p_mobile/Controller/Map.java
@@ -0,0 +1,41 @@
+package goodman.gm.p_mobile.Controller;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import goodman.gm.p_mobile.R;
+
+public class Map extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng vn = new LatLng(16.047079, 108.206230);
+        mMap.addMarker(new MarkerOptions()
+                .position(vn)
+                .title("Marker in VietNam"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(vn));
+    }
+}
